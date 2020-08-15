4c5ea7c136e0688e4b5112b4a7c9307feb6d6b41
kimizhang <kimizhang@swireproperties.com>
2020-08-15 22:13:30 +0800

    vuex模块拆分

--- src/store/store.js
+++ src/store/store.js
@@ -1,54 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import menu from './module/menu';
+import users from './module/users';
+import status from './module/status';
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state:{
-    //设置属性
-    menuItems:{},
-    currentUser:null,
-    isLogin:false
-  },
-  getters:{
-    // 获取属性的状态
-    getMenuItems: state => state.menuItems,
-    currentUser: state => state.currentUser,
-    isLogin: state => state.isLogin
-  },
-  mutations:{
-    // 改变属性的状态
-    setMenuItems(state,data){
-      state.menuItems = data
-    },
-    // 将匹配到的对象，在 menuItems 中删除
-    removeMenuItems(state,data){
-      state.menuItems.forEach( (item,index) => {
-        if (item === data){
-          state.menuItems.splice(index,1)
-        }
-      })
-    },
-    // 将新添加的 pizza push到 menuItems 属性中
-    pushToMenuItems(state,data){
-      state.menuItems.push(data)
-    },
-    // 更改用户的状态信息
-    userStatus(state,user){
-      if (user){
-        state.currentUser = user
-        state.isLogin = true
-      }else {
-        state.currentUser = null
-        state.isLogin = false
-      }
-    }
-  },
-  actions:{
-    // 应用mutations
-    setUser({commit},user){
-      commit("userStatus",user)
-    }
+  modules:{
+    menu,
+    users,
+    status
   }
 })
-
